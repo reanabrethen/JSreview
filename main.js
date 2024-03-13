34aa4a717de3fa9f46025a006389200e56c029e7
Admin <reanajb@gmail.com>
2024-03-13 09:50:43 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,61 @@
+const returnEmptyString = function () {
+
+};
+
+const returnZeroNumber = function () {};
+
+const returnEmptyArray = function () {};
+
+const returnEmptyObject = function () {};
+
+const returnString = function () {};
+
+const addition = function () {};
+
+const subtraction = function () {};
+
+const multiplication = function () {};
+
+const division = function () {};
+
+const returnArray = function () {};
+
+const returnFirstIndex = function () {};
+
+const returnSecondIndex = function () {};
+
+const returnArrayLength = function () {};
+
+const arraySum = function () {};
+
+const arraySubtraction = function () {};
+
+const multiplicationArray = function () {};
+
+const divisionArray = function () {};
+
+const oddArray = function () {};
+
+const evenArray = function () {};
+
+module.exports = {
+returnEmptyString,
+returnZeroNumber,
+returnEmptyArray,
+returnEmptyObject,
+returnString,
+addition,
+subtraction,
+multiplication,
+division,
+returnArray,
+returnFirstIndex,
+returnSecondIndex,
+returnArrayLength,
+arraySum,
+arraySubtraction,
+multiplicationArray,
+divisionArray,
+oddArray,
+evenArray
+}
